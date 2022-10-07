9f29e0b9ba76a8e7e6d1d875a54268d76dc11142
Matthew Eng <matthew.eng2@gmail.com>
2022-10-07 09:06:04 -0400

    Draw type using pixels

--- sketches/sketch-06.js
+++ sketches/sketch-06.js
@@ -4,21 +4,85 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const sketch = () => {
+let manager;
+
+let text = 'A';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
 
-    context.fillStyle = 'black';
-    context.font = '400px serif';
-    context.textBaseline = 'middle';
-    context.textAlign = 'center';
+    fontSize = cols;
 
-    context.save();
-    context.translate(width * 0.5, height * 0.5);
+    typeContext.fillStyle = 'white';
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = 'top';
 
-    context.fillText('A', 0, 0);
-    context.restore();
+
+    const metrics = typeContext.measureText(text);
+    const mx = metrics.actualBoundingBoxLeft * -1;
+    const my = metrics.actualBoundingBoxAscent * -1;
+    const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const x = (cols - mw) * 0.5 - mx;
+    const y = (rows - mh) * 0.5 - my;
+
+    typeContext.save();
+    typeContext.translate(x, y);
+
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
+
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+    typeContext.drawImage(typeCanvas, 0, 0);
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      // Read pixel channels
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+      const a = typeData[i * 4 + 3];
+
+      context.fillStyle = `rgb(${r}, ${g}, ${b})`
+
+      // Draw col x col squares across grid
+      context.save()
+      context.translate(x, y);
+      context.translate(cell * 0.5, cell * 0.5);
+      // context.fillRect(0, 0, cell, cell);
+
+      context.beginPath();
+      context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
+      context.fill();
+      context.restore()
+
+    }
   };
 };
 
